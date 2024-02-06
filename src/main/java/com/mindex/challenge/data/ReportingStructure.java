c269e75fd980a6060d645e772e0e502b24edde59
Sham Islam <si7339@g.rit.edu>
2024-02-06 16:09:25 -0500

    added the first class- ReportingStructure.java

---
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -0,0 +1,28 @@
+package com.mindex.challenge.data;
+
+public class ReportingStructure {
+    private Employee employee;
+    private int numberOfReports; 
+    
+    public ReportingStructure(Employee employee, int numberOfReports){
+    this.employee = employee;
+    this.numberOfReports = numberOfReports;  
+    
+    }
+
+    public Employee getEmployee(){
+        return this.employee;
+    }
+
+    public void setEmployee(Employee employee){
+        this.employee = employee;
+    }
+
+    public int getNumberOfReports(){
+        return this.numberOfReports;
+    }
+
+    public void setNumberOfReports(int numberOfReports){
+        this.numberOfReports = numberOfReports;
+    }
+}
